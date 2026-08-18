d9bb9da9fae87fa5ef180e82e641901fbd949b83
Montrell Cruse <montrell@rowvyn.com>
2026-08-17 21:44:04 -0700

    chore: update Homebrew formula for 0.4.3

--- Formula/servicetitan-cli.rb
+++ Formula/servicetitan-cli.rb
@@ -4,8 +4,8 @@
 class ServicetitanCli < Formula
   desc "First-party quality CLI for the ServiceTitan API"
   homepage "https://github.com/montrellcruse/servicetitan-cli"
-  url "https://registry.npmjs.org/@rowvyn/servicetitan-cli/-/servicetitan-cli-0.4.2.tgz"
-  sha256 "9ee1f0b7ff5a6176793ffb355ccb3bc5498ead7ae587d75519b8c37e3495380d"
+  url "https://registry.npmjs.org/@rowvyn/servicetitan-cli/-/servicetitan-cli-0.4.3.tgz"
+  sha256 "605336fc6874db874cfe2b5afe25aba182ff59a58610c5f79582f855dd8b46eb"
   license "MIT"
 
   depends_on "node"
@@ -17,6 +17,6 @@ class ServicetitanCli < Formula
 
   test do
     output = shell_output("#{bin}/st --version")
-    assert_match "0.4.2", output
+    assert_match "0.4.3", output
   end
 end
